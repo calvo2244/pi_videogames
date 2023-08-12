b12cea7827ddf35d327a72c6651bf4aa3a9a2709
calvo2244 <calvo2244@hotmail.com>
2023-08-12 18:21:44 -0500

    deploy client 3

--- client/src/index.js
+++ client/src/index.js
@@ -10,9 +10,7 @@ import axios from 'axios';
 
 
 // axios.defaults.baseURL= "http://localhost:3001"
-axios.defaults.baseURL= "https://pivideogames-production-6c0a.up.railway.app"
-
-
+axios.defaults.baseURL= `https://pivideogames-production-6c0a.up.railway.app`
 
 ReactDOM.render(
   <Provider store={store}>
